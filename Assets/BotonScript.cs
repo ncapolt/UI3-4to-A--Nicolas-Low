57b21adbcb8e880eeec07eb3b6831f142d8240af
ncapolt <140573792+ncapolt@users.noreply.github.com>
2024-05-27 14:28:53 -0300

    eac hecho

---
+++ Assets/BotonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonScript : MonoBehaviour
+{
+    public InputField contraseña;
+    public Text Bienvenido;
+    public string contra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Ingresecontraseña()
+    {
+    if (contraseña.text == contra)
+        {
+            Debug.Log("Access granted");
+        }
+     else
+            Debug.Log("Access denied");
+    }
+}
